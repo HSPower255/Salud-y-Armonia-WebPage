f68927358960c7742a75ceb692b89160fa80ef6d
Santiago Tourn <111823540+HSPower255@users.noreply.github.com>
2023-04-15 04:17:44 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,10 @@ export default function RootLayout({ children }) {
   return (
     <html >
       <head>
+        <title>Salud y Armonía</title>
+        <meta name="description" content="En Salud y Armonía te invitamos a vivir una experiencia de relajación total y lograr desconectar del estrés y las preocupaciones cotidianas." />
+        <meta property="og:image" content="ogimg.png"/>
+        <link rel="icon" href="logo.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap"
           rel="stylesheet"
